2e28763589220b98d729f693b348d12f31121472
خياط اليرموك <yarmoktailor@gmail.com>
2026-02-21 02:24:13 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+let db = {
+    fabrics: [
+        { title: "قماش كوري فاخر", newPrice: 15000, img: "url_here" }
+    ],
+    ready: [
+        { cat: "thobe_r", title: "ثوب خلية النحل", newPrice: 10000, img: "url_here" }
+    ]
+};
+
+let readyCartTotal = 0;
+let currentGridCols = 1;
